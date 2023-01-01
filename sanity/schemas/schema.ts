fbf295ecc0c61693546ef609e4e05b6a1f42e585
Levi <andremiracle002@gmail.com>
2023-01-01 02:03:30 +0100

    update

--- sanity/schemas/schema.ts
+++
@@ -1,7 +0,0 @@
-import pageInfo from './pageInfo'
-import skill from './skill'
-import experience from './experience'
-import social from './social'
-import project from './project'
-
-export const schemaTypes = [pageInfo, skill, experience, social, project]
